4bdee41398c488bf626082193f4aae66bcbcb7bd
rovsencode <rovshanakh@code.edu.az>
2022-08-22 22:51:59 +0400

    Project

---
+++ PistolProject/Models/Weapon.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PistolProject.Models
+{
+    class Weapon
+    {
+        private int _weaponCapacity;//gulle tutumu
+        private int _weaponBulletCount;//gulle sayi
+        private int _weaponDischargeTime;//gulle bosalma vaxti
+        private string _shootingMode; //gulle atis sekli tekli ve ya avtomatik
+
+
+        public Weapon(int weaponCapacity,int weaponBulletCount,int weaponDischargeTime,string shootingMode)
+        {
+            WeaponCapacity = weaponCapacity;
+            WeaponBulletCount = weaponBulletCount;
+            WeaponDischargeTime=weaponDischargeTime; 
+            ShootingMode = shootingMode;
+        }
+        public int WeaponCapacity { 
+            get
+            {
+                return _weaponCapacity;
+            } 
+            set
+            {
+                if (value>0)
+                {
+                    _weaponCapacity = value;
+                }
+             
+            } 
+        }
+        public int WeaponBulletCount { 
+            get
+            {
+                return _weaponBulletCount;
+            } 
+            set
+            {
+                if (value>=0 && value<= WeaponCapacity) 
+                {
+                    _weaponBulletCount = value;
+                }
+              
+            }
+        }
+        public int WeaponDischargeTime { 
+            get
+            { 
+                return _weaponDischargeTime;
+            } set
+            {
+                if (value>=0 && value<=WeaponCapacity )
+                {
+                    _weaponDischargeTime = value;
+                }
+                
+            } 
+        }
+        public string ShootingMode {
+            get {
+                return _shootingMode;
+            } 
+            set
+            {
+                if (value=="Single mode" || value=="Automatic mode" )
+                {
+                    _shootingMode = value;
+                }
+            } 
+        }
+        public  int Shoot()
+        {
+
+            if (WeaponBulletCount > 0)
+            {
+                WeaponBulletCount = WeaponBulletCount - 1;
+                Console.WriteLine("1 bullet is shooted");
+                Console.WriteLine("currenty bullets:");
+            }
+            return WeaponBulletCount;
+        }
+        public void Fire()
+        {
+            int time=0;
+            if (ShootingMode=="Automatic mode")
+            {
+                while (WeaponBulletCount > 0)
+                {
+                    WeaponBulletCount = WeaponBulletCount - 1;
+                     time++;
+
+                }
+                
+                Console.WriteLine("currenty bullets: "+ WeaponBulletCount);
+                Console.WriteLine(time + "second spent  ");
+            }
+            else if(ShootingMode=="Single mode")
+            {
+                Console.WriteLine(Shoot());
+                Console.WriteLine("1 second spent");
+            }
+             
+           
+        }
+        public int GetRemainBulletCount()
+        {
+            Console.WriteLine("remain bullets");
+            return WeaponCapacity-WeaponBulletCount;
+        }
+        public int Reload()
+        {
+            while (WeaponCapacity>WeaponBulletCount)
+            {
+                WeaponBulletCount++;
+            }
+            Console.WriteLine("weapon is reloaded ,current bullets:");
+            return WeaponBulletCount;
+        }
+        public void ChangeFireMode()
+        {
+            if (ShootingMode=="Automatic mode")
+            {
+                ShootingMode = "Single mode";
+                Console.WriteLine("mode is change");
+            }
+            else if (ShootingMode == "Single mode") 
+            {
+                ShootingMode = "Automatic mode";
+                Console.WriteLine("mode is change");
+            }
+        }
+    }
+}
---
+++ PistolProject/Program.cs
@@ -0,0 +1,96 @@
+﻿using PistolProject.Models;
+using System;
+
+namespace PistolProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+         
+        public static void MainMenu()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Please input  capacity:");
+            int weaponCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input bullet count:");
+            int weaponBulletCount = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("Please input dischargetime");
+            int weaponDischargeTime = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("please input shoot mode:Single mode or Automotic mode");
+            string shootingMode = Console.ReadLine();
+            Weapon silah = new Weapon(weaponCapacity, weaponBulletCount, weaponDischargeTime, shootingMode);
+        Input:
+            GetMenu();
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+
+
+                case 0:
+                    Console.WriteLine("\nBullet Capacity: " + silah.WeaponCapacity +
+                        "\nBullet Count: " + silah.WeaponBulletCount +
+                        "\nDischarge Time: " + silah.WeaponDischargeTime +
+                        "\nFire mode: " + silah.ShootingMode);
+                    break;
+                case 1:
+                    Console.WriteLine(silah.Shoot());
+                    break;
+                case 2:
+                    silah.Fire();
+                    break;
+                case 3:
+                    Console.WriteLine(silah.GetRemainBulletCount());
+                    break;
+                case 4:
+                    Console.WriteLine(silah.Reload());
+                    break;
+                case 5:
+                    silah.ChangeFireMode();
+                    break;
+                case 7:
+                    Console.WriteLine("please input key: \nT-new for capacity \nS-new for bullet count \nD-new for Discharge time");
+                   string key = (Console.ReadLine());
+                    if (key=="T")
+                    {
+                        Console.WriteLine("new value");
+                        silah.WeaponCapacity= Convert.ToInt32(Console.ReadLine());
+                    }
+                    else if (key=="S")
+                    {
+                        Console.WriteLine("new value ");
+                        silah.WeaponBulletCount= Convert.ToInt32(Console.ReadLine());
+                    }
+                    else if (key=="D")
+                    {
+                        Console.WriteLine("new value");
+                        silah.WeaponDischargeTime= Convert.ToInt32(Console.ReadLine()); 
+                    }
+                    break;
+                default:
+                    Console.WriteLine("\nwrong input");
+                    break;
+            }
+            if (input!=6)
+            {
+                goto Input;
+            }
+        }
+        public static void GetMenu()
+        {
+            Console.WriteLine("\nWhat are you doing?");
+            Console.WriteLine("0 -About Information Weapon");
+            Console.WriteLine("1 - Shoot");
+            Console.WriteLine("2 - Fire");
+            Console.WriteLine("3 -  Requried bullet count for reolad");
+            Console.WriteLine("4 - Reolad bullet ");
+            Console.WriteLine("5 -Change fire mode ");
+            Console.WriteLine("6 - Exit from program");
+            Console.WriteLine("7 - Do edit:");
+          
+
+        }
+    }
+}
